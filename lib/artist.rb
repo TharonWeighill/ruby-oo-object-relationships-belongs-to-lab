ed164e9e9ff00e0f62d7978f86f77501dbcc9ed7
tharon <tharonweighill@gmail.com>
2020-11-21 12:45:40 -0700

    commit tree 51d71f3e4ada2e71c904103d234fb1fd71d05018

---
+++ lib/artist.rb
@@ -0,0 +1,7 @@
+class Artist
+    attr_accessor :name
+    def Artist.name(name)
+      @name = name
+    end
+end 
+
---
+++ lib/post.rb
@@ -0,0 +1,7 @@
+class Post
+    attr_accessor :title, :author 
+    def Post.title(title)
+        @title = title 
+    end
+
+end
